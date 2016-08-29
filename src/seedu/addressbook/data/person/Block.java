ae07348a3317ca67c8845405a402c9fa0953eeff
yyy.com <shawn961025@gmail.com>
2016-08-30 00:30:17 +0800

    "Block" class added

--- src/seedu/addressbook/data/person/Block.java
+++ src/seedu/addressbook/data/person/Block.java
@@ -1,5 +1,18 @@
 package seedu.addressbook.data.person;
 
-public class Block {
+/**
+ * Represents a Person's block name.
+ * Guarantees: immutable; is valid as declared in {@link #isValidAddress(String)}
+ */
 
+public class Block {
+	private final String value;
+	
+	public Block (String blockName) {
+		this.value = blockName;
+	}
+	
+	public String getBlock () {
+		return this.value;
+	}
 }
